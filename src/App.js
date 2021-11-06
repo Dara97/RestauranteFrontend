c06d2b70e299b0d2c63d436d01d0981eb4cc3870
Dara97 <dara.inampuez@gmail.com>
2021-11-06 18:39:42 -0500

    Primer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { Redirect, Route, Switch } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
+import { Home } from "./pages/Home/home";
+import { Nosotros } from "./pages/Nosotros/nosotros";
+import { Servicios } from "./pages/Servicios/servicios";
+
+import { MyModal } from "./components/MyModal/mymodal";
+import { AppProvider } from "./context/AppContext";
+import { Menu } from "./pages/Menu/menu";
+import { OrderInfo } from "./pages/OrderInfo/orderinfo";
+import { ShoppingCart } from "./pages/ShoppingCart/shoppingcart";
+
+import { Form } from "./pages/Contact/formularioReserva";
+import { FormContact } from "./pages/Contact/formularioContactanos";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+
+import Layout from "./components/Loyout";
+
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppProvider>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/servicios" component={Servicios} />
+            <Route exact path="/nosotros" component={Nosotros} />
+            <Route exact path="/menu" component={Menu} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/formReserva" component={Form} />
+            <Route exact path="/formContactanos" component={FormContact} />
+            <Route exact path="/registro" component={Register} />
+            <Route exact path="/menu" component={Menu} />
+            <Route exact path="/carrito" component={ShoppingCart} />
+            <Route exact path="/orden" component={OrderInfo} />
+            <Redirect to="/" />
+          </Switch>
+          <MyModal />
+        </Layout>
+      </BrowserRouter>
+    </AppProvider>
   );
-}
-
-export default App;
+};
