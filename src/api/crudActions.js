c4bbc2c6d933b289431987471931ba30f1219216
Victor200223 <91858414+Victor200223@users.noreply.github.com>
2021-11-22 00:08:46 -0500

    Se agregan funciones para pedir datos al backend

---
+++ src/api/crudActions.js
@@ -0,0 +1,55 @@
+import { useCallback, useEffect, useState } from "react";
+
+const API_URL = "http://localhost:41399/api";
+
+export const useGet = (entity, { onCompleted } = {}) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = useCallback(async () => {
+    setLoading(true);
+    const res = await fetch(`${API_URL}/${entity}`);
+    const resData = await res.json();
+    if (onCompleted) {
+      onCompleted(resData);
+    }
+    setData(resData);
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return { data, loading, refetch: fetchData };
+};
+
+export const deleteRecord = async (entity, id) => {
+  try {
+    const res = await fetch(`${API_URL}/${entity}/${id}`, {
+      method: "DELETE",
+    });
+
+    if (res.ok) {
+      alert(`${entity} eliminado exitosamente`);
+    }
+  } catch (error) {
+    alert(`Error al eliminar el ${entity}`);
+  }
+};
+
+export const createRecord = async (entity, body) => {
+  try {
+    await fetch(`${API_URL}/${entity}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    alert("Comentario creado exitosamente");
+  } catch (error) {
+    alert(`Error al eliminar el ${entity}`);
+  }
+};
